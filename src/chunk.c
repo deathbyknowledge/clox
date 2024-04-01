f0efe27e0f564a248aa613560292e97096b9174b
Steve James <0x2t1ff@gmail.com>
2024-04-01 10:58:40 +0200

    improved line count

--- src/chunk.c
+++ src/chunk.c
@@ -1,32 +1,93 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "chunk.h"
 #include "memory.h"
 
+
+/* LINE COUNT */
+
+static uint16_t encodeLine(int line, int count) {
+   uint16_t lineNum = (uint16_t) ((unsigned int) line);
+   uint16_t c = (uint16_t) ((unsigned int) count);
+   return ((c << 11) + (lineNum & 2047));
+}
+
+static void decodeLine(uint16_t line, int* lineNum, int* count) {
+   *count = (int)(line >> 11);
+   *lineNum = (int)(line & 2047);
+}
+
+void initLineCount(LineCount* lc) {
+  lc->count = 0;
+  lc->capacity = 0;
+  lc->lines = NULL;
+}
+
+void freeLineCount(LineCount* lc) {
+  FREE_ARRAY(uint16_t, lc->lines, lc->capacity);
+  initLineCount(lc);
+}
+
+void writeLineCount(LineCount* lc, int line) {
+  // Check if Op array needs to be grown.
+  int prevCount, prevLineNum;
+if (lc->capacity == 0) {
+    lc->capacity = GROW_CAPACITY(0);
+    lc->lines = GROW_ARRAY(uint16_t, lc->lines, 0, lc->capacity);
+    lc->lines[lc->count] = encodeLine(line, 1);
+    lc->count++;
+    return;
+  }
+
+  decodeLine(lc->lines[lc->count - 1], &prevLineNum, &prevCount);
+
+  // Make sure the counter still fits in 4 bits.
+  if (prevLineNum == line && prevCount < 15) {
+    prevCount++;
+    lc->lines[lc->count - 1] = encodeLine(prevLineNum, prevCount);
+    return;
+  }
+
+  // Check if Line array needs to be grown.
+  if (lc->capacity < lc->count + 1) {
+    int oldCapacity = lc->capacity;
+    lc->capacity = GROW_CAPACITY(oldCapacity);
+    lc->lines = GROW_ARRAY(uint16_t, lc->lines, oldCapacity, lc->capacity);
+  }
+
+  lc->lines[lc->count] = encodeLine(line, 1);
+  lc->count++;
+
+}
+
+
+/* CHUNK */
 void initChunk(Chunk* chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
-  chunk->lines = NULL;
+  initLineCount(&chunk->lines);
   initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk* chunk) {
   FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);    
-  FREE_ARRAY(int, chunk->lines, chunk->capacity);    
+  freeLineCount(&chunk->lines);
   freeValueArray(&chunk->constants);
   initChunk(chunk);
 }
 
 void writeChunk(Chunk* chunk, uint8_t byte, int line) {
+  // Check if Op array needs to be grown.
   if (chunk->capacity < chunk->count + 1) {
     int oldCapacity = chunk->capacity;
     chunk->capacity = GROW_CAPACITY(oldCapacity);
     chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
-    chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
   }
+
   chunk->code[chunk->count] = byte;
-  chunk->lines[chunk->count] = line;
+  writeLineCount(&chunk->lines, line);
   chunk->count++;
 }
 
@@ -35,6 +96,17 @@ int addConstant(Chunk* chunk, Value value) {
   return chunk->constants.count - 1;
 }
 
-int getLine(Chunk* chunk, int inst_idx) {
+
+int getLine(Chunk* chunk, int instIdx) {
+  int count = 0, idx = 0;
    
+  while (true) {
+    int lineNum, lineCount;
+    decodeLine(chunk->lines.lines[idx], &lineNum, &lineCount);
+    if (instIdx >= count && instIdx <= (count+lineCount - 1)) {
+      return lineNum;
+    }
+    idx++;
+    count += lineCount;
+  }
 }
--- src/chunk.h
+++ src/chunk.h
@@ -12,15 +12,31 @@ typedef enum {
 typedef struct {
   int count;
   int capacity;
+  // might want to swap this to unsigned too
+  uint16_t* lines;  //(TODO: revisit encoding) [15:12]: instruciton index - [11:0]: line number 
+} LineCount;
+
+typedef struct {
+  int count;
+  int capacity;
   uint8_t* code;
-  int* lines;
+
+  int linesCount;
+  int linesCapacity;
+  LineCount lines;
   ValueArray constants;
 } Chunk;
+
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
 int getLine(Chunk* chunk, int inst_idx);
+
+void initLineCount(LineCount* lc);
+void freeLineCount(LineCount* lc);
+void writeLineCount(LineCount* lc, int line);
+
 
 #endif
