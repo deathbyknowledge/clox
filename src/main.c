64b32d48444c6a1a3bc1ab0a76a71ee3e9bae917
Steve James <0x2t1ff@gmail.com>
2024-04-02 12:23:35 +0200

    add vm with stack

--- src/main.c
+++ src/main.c
@@ -1,18 +1,20 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+  initVM();
+
   Chunk chunk;
   initChunk(&chunk);
 
-  writeChunk(&chunk, OP_RETURN, 123);
-  for (int i = 0; i <= 257; i++) {
-    writeConstant(&chunk, i, 420);
-  }
   writeConstant(&chunk, 69, 33);
-  disassembleChunk(&chunk, "test chunk");
+  writeChunk(&chunk, OP_RETURN, 123);
+  //disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
 
+  freeVM();
   freeChunk(&chunk);
   return 0;
 }
