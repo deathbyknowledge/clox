e0267f5241d1ac2b105a7de91d53a4c0c9d999bf
Steve James <0x2t1ff@gmail.com>
2024-04-02 20:27:30 +0200

    now we have a scanner

--- src/vm.h
+++ src/vm.h
@@ -22,8 +22,8 @@ typedef enum {
 
 void initVM();
 void freeVM();
-InterpretResult interpret(Chunk* chunk);
 void push(Value value);
+InterpretResult interpret(const char* source);
 Value pop();
 
 #endif
