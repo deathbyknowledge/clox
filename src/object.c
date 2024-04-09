4e121da006996eb45ff19c3561bab6c17d23a22b
Steve James <0x2t1ff@gmail.com>
2024-04-09 13:39:40 +0200

    ObjString chars is now a flexible field array
    This might bite me in the ass in the future. Dereferencing chars from a Value should be faster now but unsure if I implemented the changes without any UB.

--- src/object.c
+++ src/object.c
@@ -17,22 +17,22 @@ static Obj* allocateObject(size_t size, ObjType type) {
   return obj;
 }
 
-static ObjString* allocateString(char* chars, int length) {
-  ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
+static ObjString* allocateString(const char* chars, int length) {
+  ObjString* string = (ObjString*)allocateObject(sizeof(ObjString) + length * sizeof(char), OBJ_STRING);
   string->length = length;
-  string->chars = chars;
+  memcpy(string->chars, chars, string->length);
   return string;
 }
 
 ObjString* takeString(char* chars, int length) {
-  return allocateString(chars, length);
+  ObjString* str = allocateString((const char*)chars, length);
+  // AllocateString copies the chars so we free them after.
+  FREE_ARRAY(char, chars, length);
+  return str;
 }
 
 ObjString* copyString(const char* chars, int length) {
-  char* heapChars = ALLOCATE(char, length + 1);
-  memcpy(heapChars, chars, length);
-  heapChars[length] = '\0';
-  return allocateString(heapChars, length);
+  return allocateString(chars, length);
 }
 
 void printObject(Value value) {
